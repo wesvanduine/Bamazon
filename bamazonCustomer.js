150cf2b4d0e9db3c091984c5e72f109ffc812a17
Wes Van Duine <Wes Van Duine>
2017-07-09 20:28:20 -0700

    sql database and table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const Table = require('cli-table');
 
 //Input connection info for mySQL database
 const connection = mysql.createConnection({
@@ -9,9 +10,25 @@ const connection = mysql.createConnection({
     database: 'bamazon_DB'
 });
 
-
 //Connection to the database
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-})
+    //connect to the mysql database and pull the information from the Products database to display to the user
+    connection.query('SELECT id, product_name, department_name, price, stock_quantity FROM Products', function(err, result) {
+        if (err) console.log(err);
+
+        //creates a table for the information from the mysql database to be placed
+        var table = new Table({
+            head: ['ItemID', 'Product Name', 'Department', 'Price', 'Stock'],
+        });
+        //loops through each item in the mysql database and pushes that information into a new row in the table
+        for (var i = 0; i < result.length; i++) {
+            table.push(
+                [result[i].id, result[i].product_name, result[i].department_name, result[i].price, result[i].stock_quantity]
+            );
+        }
+        console.log(table.toString());
+    });
+
+});
--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -15,4 +15,4 @@ CREATE TABLE products (
 
 -- Insert 10 mock items into the table
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES('Iphone', 'Electronics', 650.00, 45), ('Soap', 'kitchen', 9.50, 50), ('Amazon Echo', 'Electronics', 89.00, 24), ('Camping Charis', 'Outdoors', 15.00, 15), ('Bose Headphones', 'Electronics', 15.00, 30), ('Rice Maker', 'Kitchen', 40.00, 10), ('Running Watch', 'Outdoors', 130.00, 25), ('Kitchen Towels', 'Kitchen', 40.00, 10), ('Laptop', 'Electronics', 550.00, 8), ('Storage Shed', 'Outdoors', 100.00, 5); 
+VALUES('Iphone', 'Electronics', 650.00, 45), ('Soap', 'kitchen', 9.50, 50), ('Amazon Echo', 'Electronics', 89.00, 24), ('Camping Chair', 'Outdoors', 15.00, 15), ('Bose Headphones', 'Electronics', 15.00, 30), ('Rice Maker', 'Kitchen', 40.00, 10), ('Running Watch', 'Outdoors', 130.00, 25), ('Kitchen Towels', 'Kitchen', 40.00, 10), ('Laptop', 'Electronics', 550.00, 8), ('Storage Shed', 'Outdoors', 100.00, 5); 
